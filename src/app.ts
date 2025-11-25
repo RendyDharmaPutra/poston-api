048801d1bc05ebe468cff7b53c6e974498c83230
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-11-25 23:14:50 +0700

    feat: implementing custom api response & new default custom error handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,37 @@
-import { Hono } from "hono";
+/**
+ * This module is the entry point of the application. It creates a Hono application
+ * instance and sets up the error handling middleware. It also defines a route for
+ * the root path ("/") that returns a success response with the message "Hello Hono!".
+ *
+ * @module app
+ */
 
+import { Hono } from "hono";
+import { errorHandler } from "./common/exception/error_handler";
+import { response } from "./common/utils/response";
+
+/**
+ * The Hono application instance.
+ *
+ * @constant
+ * @type {Hono}
+ */
 export const app = new Hono().basePath("/api");
 
+/**
+ * Sets up the error handling middleware for the application.
+ */
+app.onError(errorHandler);
+
+/**
+ * Defines a route for the root path ("/") that returns a success response with the
+ * message "Hello Hono!".
+ *
+ * @name GET /
+ * @function
+ * @param {Context} c - The Hono context object.
+ * @returns {Promise<void>} A promise that resolves with the updated Hono context object.
+ */
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return response.success(c, "Hello Hono!");
 });
